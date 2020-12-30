fc060943d56daeebe1e1cb9671bc9884cdef3df5
Al Mo <admin@Als-MacBook-Pro.local>
2020-12-30 11:50:50 -0500

    implemented SSL redirect

--- server.js
+++ server.js
@@ -14,3 +14,21 @@ res.sendFile(path.join(__dirname+'/dist/spectra-app/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
+/* This code below is just to force SSL usage*/
+/* If an incoming request uses a protocol other than HTTPS, redirect that request to the same url but with HTTPS*/
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }
+  /* Instruct the app to use the forceSSL middleware*/
+  app.use(forceSSL());
+  
+
