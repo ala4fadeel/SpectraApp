019e624167afefdc0d3dd9da501fd6bd49bdba0a
Al Mo <admin@Als-MacBook-Pro.local>
2020-12-30 12:32:03 -0500

    Adding SSL 1st

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const path = require('path');
 
 const app = express();
 
+
+/* This code below is just to force SSL usage*/
+/* If an incoming request uses a protocol other than HTTPS, redirect that request to the same url but with HTTPS*/
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+/* Instruct the app to use the forceSSL middleware*/
+app.use(forceSSL());
+
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/spectra-app'));
 
@@ -16,19 +33,4 @@ res.sendFile(path.join(__dirname+'/dist/spectra-app/index.html'));
 app.listen(process.env.PORT || 8080);
 
 
-/* This code below is just to force SSL usage*/
-/* If an incoming request uses a protocol other than HTTPS, redirect that request to the same url but with HTTPS*/
-const forceSSL = function() {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }
-  }
-  /* Instruct the app to use the forceSSL middleware*/
-  app.use(forceSSL());
-  
 
